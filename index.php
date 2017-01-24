1ccea7e050e630d349c4cf22177dc318056942c5
Aleks <aleks@xanti.co>
2017-01-24 21:05:42 +0800

    designing form

--- index.php
+++ index.php
@@ -9,27 +9,35 @@
     </head>
 
     <body>
-        <div class="container">
-            <form class="form-horizontal">
-                <div class="form-group">
-                    <label for="inputName" class="col-sm-2 control-label">Name</label>
-                    <div class="col-sm-10">
-                        <input type="name" class="form-control" id="inputName" placeholder="">
+        <div class="container ">
+            <div class="sign-up-container ">
+                <form class="form-horizontal col-md-5">
+                    <div class="form-heading">
+                        <h2>Sign-Up</h2>
+                        <span>Complete the following fields</span>
                     </div>
-                </div>
-                <div class="form-group">
-                    <label for="inputEmail3" class="col-sm-2 control-label">Birthday</label>
-                    <div class="col-sm-10">
-                        <input type="birthday" class="form-control" id="inputBirthday" placeholder="">
+                    <hr>
+                    <div class="form-group">
+                        <label for="inputName" class="col-sm-2 control-label">Name</label>
+                        <div class="col-sm-10">
+                            <input type="name" class="form-control" id="inputName" placeholder="">
+                        </div>
                     </div>
-                </div>
-                
-                <div class="form-group">
-                    <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-default">Sign in</button>
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Birthday</label>
+                        <div class="col-sm-10">
+                            <input type="birthday" class="form-control" id="inputBirthday" placeholder="">
+                        </div>
                     </div>
-                </div>
-            </form>
+
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-default">Submit</button>
+                            <button type="submit" class="btn btn-default">Export to XML</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </body>
 
