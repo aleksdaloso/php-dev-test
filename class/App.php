ca53b2245daa500edc6bd98d30d14bdf2533e278
Aleks <aleks@xanti.co>
2017-01-25 17:20:34 +0800

    added validation and generate xml/text files

---
+++ class/App.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aleksd
+ * Date: 1/25/17
+ * Time: 12:28 PM
+ */
+class App
+{
+
+}
---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aleksd
+ * Date: 1/25/17
+ * Time: 12:30 PM
+ */
